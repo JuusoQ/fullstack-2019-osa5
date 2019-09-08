9af829c3c61870cd22567c401104e999a857ddc8
Juuso Haapanen <juuso@haapanen.biz>
2019-09-08 20:01:55 +0300

    Tehtävä 5.6 DONE

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    setVisible(!visible);
+  }
+
+  const showDetails = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    border: 1,
+    paddingLeft:10,
+    paddingTop:5,
+    borderWidth: 1,
+    marginBottom: 1
+  }
+
+  return (
+
+    <div style={blogStyle}>
+      <div onClick={() => toggleVisible()} >{blog.title}</div>
+
+      <div style={showDetails}>
+        {blog.title} <br/>Author: {blog.author} <br/> Url: {blog.url}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
